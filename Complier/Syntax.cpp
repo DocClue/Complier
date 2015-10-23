df1016e7b2233cf0da9a95494c12c6d8615e3357
Dr.Clue <twilight71777@gmail.com>
2015-10-23 15:22:52 -0600

    Sytnax File

---
+++ Complier/Syntax.cpp
@@ -0,0 +1,9 @@
+//
+//  Syntax.cpp
+//  Complier
+//
+//  Created by Timothy Hartt on 10/23/15.
+//  Copyright (c) 2015 The Sith Lords. All rights reserved.
+//
+
+#include "Syntax.h"
---
+++ Complier/Syntax.h
@@ -0,0 +1,14 @@
+//
+//  Syntax.h
+//  Complier
+//
+//  Created by Timothy Hartt on 10/23/15.
+//  Copyright (c) 2015 The Sith Lords. All rights reserved.
+//
+
+#ifndef __Complier__Syntax__
+#define __Complier__Syntax__
+
+#include <stdio.h>
+
+#endif /* defined(__Complier__Syntax__) */
