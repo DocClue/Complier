d08f6da91033339efad28a798464b8b913ca0ff3
Dr.Clue <twilight71777@gmail.com>
2015-10-23 15:08:31 -0600

    Initial Commit

---
+++ Complier/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Complier
+//
+//  Created by Timothy Hartt on 10/23/15.
+//  Copyright (c) 2015 The Sith Lords. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
