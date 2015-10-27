618e6e1cc15eccc7e5b8016a2048ff7124e48892
Dr.Clue <twilight71777@gmail.com>
2015-10-27 14:50:42 -0600

    ss

--- Complier/PreProc.cpp
+++ Complier/PreProc.cpp
@@ -8,18 +8,22 @@ std::string PreProcess(std::string file)
 {
     std::ifstream ifs(file, std::ifstream::in);
     std::string lineInfo;
-    
+    std::string command ;
+    int a = 0;
     while (ifs) {
+        
         getline(ifs,lineInfo);
         for (auto i = lineInfo.cbegin(); i!= lineInfo.cend(); i++) {
             if (*i == ' ') {
                 
-            } else if (*i =='#') {
                 
+            } else if (*i =='#') {
+                command += *i;
                 
             } else {
                 // No preprocessor command on this line.
             }
+            a++;
         }
         
     }
